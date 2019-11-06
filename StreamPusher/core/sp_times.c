05c32983e52db7dc97545d82698f83ef41d08e42
1264221994@qq.com <1264221994@qq.com>
2019-11-06 22:51:21 +0800

    add slot in times files

--- StreamPusher/core/sp_times.c
+++ StreamPusher/core/sp_times.c
@@ -1,1 +1,13 @@
 #pragma once
+#include "sp_times.h"
+
+#define SP_TIME_SLOTS   64
+
+static sp_uint_t slot;
+static sp_atomic_t sp_time_lock;
+
+
+
+static char  *week[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+static char  *months[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+						   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
--- StreamPusher/core/sp_times.h
+++ StreamPusher/core/sp_times.h
@@ -1,1 +1,15 @@
 #pragma once
+#ifndef _SP_TIMES_H_INCLUDED_
+#define _SP_TIMES_H_INCLUDED_
+
+#include "sp_core.h"
+
+
+typedef struct {
+	time_t      sec;
+	sp_uint_t  msec;
+	sp_int_t   gmtoff;
+} sp_time_t;
+
+
+#endif // !_SP_TIMES_H_INCLUDED_
