fccd8dc66a8f0429ff411ed2156e721a0d259b90
weichao-kooboo <1264221994@qq.com>
2020-11-17 23:11:06 +0800

    delete files

--- test/AvioReading.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class AvioReading
-{
-public:
-	AvioReading();
-	~AvioReading();
-	int run(int argc,const char *argv[]);
-};
-
--- webSite/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\wc132\PycharmProjects\untitled1\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
